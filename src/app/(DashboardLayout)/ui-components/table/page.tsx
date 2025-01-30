eef472fd05be0dd985827c83bede222136938e58
inadrayanikalke <indrayanikalake13@gmail.com>
2025-01-30 19:04:43 +0530

    files removed

--- src/app/(DashboardLayout)/ui-components/table/page.tsx
+++ src/app/(DashboardLayout)/ui-components/table/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { Grid } from "@mui/material";
 import ProductPerfomance from "@/app/(DashboardLayout)/components/dashboard/ProductPerformance";
-import Table1 from "../../components/tables/Table1";
 
 const Tables = () => {
   return (
     <Grid container spacing={0}>
       <Grid item xs={12} lg={12}>
-        {/* <ProductPerfomance /> */}
-        <Table1 />
+        <ProductPerfomance />
       </Grid>
     </Grid>
   );
